9f5e0fd214ae85761f5b52d10430601217c2240d
David <ByZorks@users.noreply.github.com>
2025-07-09 18:53:19 +0200

    triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <ostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, char *argv[])
@@ -7,26 +10,43 @@ int main(int argc, char *argv[])
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "GLCraft", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    if (glewInit() != GLEW_OK) std::cout << "glewInit() failed" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    constexpr float vertices[] = {
+        // x, y, z
+        0.0f, .5f, 0.0f,
+        .5f, -.5f, 0.0f,
+        -.5f, -.5f, 0.0f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+    glfwMakeContextCurrent(window);
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Poll for and process events */
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
