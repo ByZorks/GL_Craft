23c5a59abe8cde3cac89850f3706720d1b275b26
David <ByZorks@users.noreply.github.com>
2025-07-19 21:26:15 +0200

    Remove redundant includes in Camera.cpp

--- src/render/Camera.cpp
+++ src/render/Camera.cpp
@@ -1,12 +1,4 @@
 #include "Camera.h"
-#include "Camera.h"
-#include "Camera.h"
-#include "Camera.h"
-#include "Camera.h"
-#include "Camera.h"
-#include "Camera.h"
-#include "Camera.h"
-#include "Camera.h"
 
 #include <cmath>
 
