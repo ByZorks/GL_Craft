2ca6086cff825f91fe5fbafc5230e57df31b568c
David <ByZorks@users.noreply.github.com>
2025-07-26 23:15:25 +0200

    Increase default camera field of view to 70 degrees for wider perspective

--- src/render/Camera.cpp
+++ src/render/Camera.cpp
@@ -13,7 +13,7 @@ Camera::Camera(const unsigned int windowWidth,
                                                   m_firstMouse(true), m_cameraPos(glm::vec3(0.0f, 110, .0f)),
                                                   m_cameraFront(glm::vec3(0.0f, 0.0f, -1.0f)),
                                                   m_cameraUp(glm::vec3(0.0f, 1.0f, 0.0f)),
-                                                  m_FOVDegrees(45.f),
+                                                  m_FOVDegrees(70.f),
                                                   m_aspectRatio(
                                                       static_cast<float>(windowWidth) / static_cast<float>(
                                                           windowHeight)),
