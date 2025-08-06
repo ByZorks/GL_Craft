950227272f6a7bcd2c09ee94626da47380ee2dbe
David <ByZorks@users.noreply.github.com>
2025-08-07 01:48:59 +0200

    Add TODO comments to optimize distance calculations in Camera class

--- src/render/Camera.cpp
+++ src/render/Camera.cpp
@@ -86,6 +86,7 @@ void Camera::mouseCallback(GLFWwindow *window, const double xpos, const double y
     cam->handleMouse(xpos, ypos);
 }
 
+// TODO: Use squared distance to avoid sqrt calculation
 float Camera::distanceToCamera(const Mesh& mesh) const {
     const auto x = static_cast<float>(mesh.m_x1());
     const auto y = static_cast<float>(mesh.m_y1());
@@ -96,6 +97,7 @@ float Camera::distanceToCamera(const Mesh& mesh) const {
     return glm::distance(m_cameraPos, farCorner);
 }
 
+// TODO: Use squared distance to avoid sqrt calculation
 float Camera::distanceToCamera(const glm::vec3 position) const {
     return glm::distance(m_cameraPos, position);
 }
