817dd7c780307c7d0caf3446b7a285c8a0fd9714
David <ByZorks@users.noreply.github.com>
2025-08-27 23:00:22 +0200

    Add texture mipmap support

--- src/gl/Texture.cpp
+++ src/gl/Texture.cpp
@@ -27,16 +27,7 @@ Texture::Texture(std::string filePath) : m_FilePath(std::move(filePath)), m_Loca
 
     GLCall(glTextureStorage2D(m_ID, 1, GL_RGBA8, m_Width, m_Height));
     GLCall(glTextureSubImage2D(m_ID, 0, 0, 0, m_Width, m_Height, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer));
-
-    // GLCall(glGenTextures(1, &m_ID));
-    // GLCall(glBindTexture(GL_TEXTURE_2D, m_ID));
-    //
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
-    //
-    // GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer));
+    GLCall(glGenerateTextureMipmap(m_ID));
 
     if (m_LocalBuffer) stbi_image_free(m_LocalBuffer);
 }
